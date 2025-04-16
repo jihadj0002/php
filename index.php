efec8eabe5a4f27220905f73778873561ec6082b
jihadj0002 <87649538+jihadj0002@users.noreply.github.com>
2025-04-17 00:29:09 +0600

    Created Index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+$txt = "Hello world <br>";
+echo "$txt <br>";
+
+function myName($name) {
+    echo "My name is: $name";
+}
+
+myName("Jihad");
+?>
+
+
+</body>
+</html>
